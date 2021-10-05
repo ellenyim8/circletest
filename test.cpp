f92b19a466a3918b7094b34ee309e679e649919d
ellenyim8 <eyim009@ucr.edu>
2021-10-04 22:45:06 -0700

    edited test file

--- test.cpp
+++ test.cpp
@@ -8,7 +8,7 @@ TEST(Area, AreaIsCorrect)
         circ.set_radius(3);
         EXPECT_DOUBLE_EQ(circ.area(), 3 * 3 * M_PI);
         ASSERT_NEAR(circ.area(), 28.274333, 0.3); 
-	//ASSERT_EXIT(AreaIsCorrect, ::testing::ExitedWithCode(0), "Success");
+	//EXPECT_DEATH(::testing::ExitedWithCode(0), "");
 }
 
 TEST(Perimeter, PerimeterIsCorrect)
@@ -18,7 +18,7 @@ TEST(Perimeter, PerimeterIsCorrect)
         ASSERT_EQ(c.get_radius(), 4);
         EXPECT_DOUBLE_EQ(c.perimeter(), 2 * M_PI * 4);
         ASSERT_NEAR(c.perimeter(), 25.13274122, 0.1);
-	//ASSERT_EXIT(PerimeterIsCorrect, ::testing::ExitedWithCode(0), "Success");
+	
 }
 
 TEST(Radius, ValidRadius)
@@ -28,7 +28,8 @@ TEST(Radius, ValidRadius)
         c.set_radius(5);
         ASSERT_EQ(c.get_radius(), 5);
         exit(1);
-	//ASSERT_EXIT(ValidRadius, ::testing::ExitedWithCode(0), "Success");
+	EXPECT_DEATH(::testing::ExitedWithCode(0), "");	
+
 }
 
 int main(int ac, char* av[])
